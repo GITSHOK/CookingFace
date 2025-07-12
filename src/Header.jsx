db3023cb5e56938f1ed79f02c634b6a84ae4d1ba
maheswari12340 <nishokganapathy07@gmail.com>
2025-07-12 22:57:35 +0530

    added some infos

--- src/Header.jsx
+++ src/Header.jsx
@@ -5,6 +5,7 @@ export default function Header(){
         <header>
             <img className='logo' src={chefClaude} alt="" />
             <h1>Cooking Face</h1>
+            <p>Made using Mistralai LLM. Version 0.1</p>
         </header>
     )
 }
